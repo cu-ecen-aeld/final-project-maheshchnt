f24e3359fe8240a1c05408ac508294fec98df1a3
maheshchnt <maheshchnt@gmail.com>
2022-11-13 15:54:30 -0700

    commiting additional changes

---
+++ base_external/package/aesd-project/aesd-project.mk
@@ -0,0 +1,29 @@
+
+##############################################################
+#
+# AESD-PROJECT
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+AESD_PROJECT_VERSION = e7ecc297f5fd1d74e026321db82f81ca5e1582f8
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESD_PROJECT_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-maheshchnt.git
+AESD_PROJECT_SITE_METHOD = git
+AESD_PROJECT_GIT_SUBMODULES = YES
+
+define AESD_PROJECT_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+endef
+
+define AESD_PROJECT_INSTALL_TARGET_CMDS
+	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+        $(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/server/S99aesdsocket.sh $(TARGET_DIR)/etc/init.d
+endef
+
+$(eval $(generic-package))
